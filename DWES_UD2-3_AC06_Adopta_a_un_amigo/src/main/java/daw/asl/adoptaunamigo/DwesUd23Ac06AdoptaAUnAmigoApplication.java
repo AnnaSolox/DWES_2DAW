7aed5899f1a40995536927d3d072a87a60a3f8fe
AnnaSolox <anna.sol.loz@gmail.com>
2025-11-11 19:03:41 +0100

    new exercise

---
+++ DWES_UD2-3_AC06_Adopta_a_un_amigo/src/main/java/daw/asl/adoptaunamigo/DwesUd23Ac06AdoptaAUnAmigoApplication.java
@@ -0,0 +1,13 @@
+package daw.asl.adoptaunamigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwesUd23Ac06AdoptaAUnAmigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwesUd23Ac06AdoptaAUnAmigoApplication.class, args);
+    }
+
+}
---
+++ DWES_UD2-3_AC06_Adopta_a_un_amigo/src/test/java/daw/asl/adoptaunamigo/DwesUd23Ac06AdoptaAUnAmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package daw.asl.adoptaunamigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwesUd23Ac06AdoptaAUnAmigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
