7eb952cc0a2e4f2daba35a17a501a279e13089c5
AnnaSolox <anna.sol.loz@gmail.com>
2025-11-11 19:08:39 +0100

    nueva carpeta ejercicio 6

---
+++ DWES_UD2-3_AC06_Adopta_a_un_amigo/src/main/java/daw/asl/adopta_a_un_amigo/AdoptaAUnAmigoApplication.java
@@ -0,0 +1,13 @@
+package daw.asl.adopta_a_un_amigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaAUnAmigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoptaAUnAmigoApplication.class, args);
+    }
+
+}
---
+++ DWES_UD2-3_AC06_Adopta_a_un_amigo/src/test/java/daw/asl/adopta_a_un_amigo/AdoptaAUnAmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package daw.asl.adopta_a_un_amigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaAUnAmigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
